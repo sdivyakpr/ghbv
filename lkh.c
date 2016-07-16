1622e1e0639a2489998b2183c0c12443930974f9
sdivyakpr <indhusachithanantham@gmail.com>
2016-07-16 15:09:13 +0530

    finish

--- lkh.c
+++ lkh.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 int main(void) {
-	int a[10],b[10],sum[10];
+	int a[12],b[12],sum[12];
 	int i;
-  printf("\n Enter the integer array values for a:");
-  for(i=0;i<10;i++)
+  printf("\n Enter the  a:");
+  for(i=0;i<12;i++)
   scanf("%d",&a[i]);
-   printf("\n Enter the integer array values for b:");
-  for(i=0;i<10;i++)
+   printf("\n Enter the b:");
+  for(i=0;i<12;i++)
   scanf("%d",&b[i]);
-   for(i=0;i<10;i++)
+   for(i=0;i<12;i++)
  sum[i]=a[i]+b[i];
  printf("\n Sum of array is :");
- for(i=0;i<10;i++)
+ for(i=0;i<12;i++)
  printf("\t%d",sum[i]);
  	return 0;
 }
